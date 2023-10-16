88fe9aa7488a888cec12b0d62fd0135ca6292a1a
Jorge Carranza <73572833+JorgeCQ07@users.noreply.github.com>
2023-10-15 21:43:14 -0600

    Corrige id

--- src/layouts/billing/components/Invoices/index.js
+++ src/layouts/billing/components/Invoices/index.js
@@ -15,17 +15,13 @@ import Invoice from "layouts/billing/components/Invoice";
 //Firebase crud
 import * as firebase from '../../../../services/billing.js'
 
-function Invoices() {
+function Invoices({ values }) {
 
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    const currentDate = new Date().toLocaleDateString();
-    //@TODO: Cambiar el id por el id del usuario logueado
-    firebase.getBillingOrderByDate("1", currentDate).then((data) => {
-      setData(data);
-    });
-  }, []);
+    setData(values);
+  }, [values]);
 
   return (
     <Card id="delete-account" sx={{ height: "90%" }}>
@@ -37,9 +33,9 @@ function Invoices() {
           onClick={() => {
             firebase.postBilling("Pago", 1000, "10/14/2023", "Ingreso", "1");
             //@TODO: Esto al guardar en el modal
-            firebase.getBilling("1").then((data) => {
-              setData(data);
-            });
+            // firebase.getBilling("1").then((data) => {
+            //   setData(data);
+            // });
           }}
         >
           Nuevo
@@ -47,17 +43,16 @@ function Invoices() {
       </VuiBox>
       <VuiBox>
         <VuiBox component="ul" display="flex" flexDirection="column" p={0} m={0}>
-          {data.map((item, index) => (
-            index < 3 ?
-              item.type === "Ingreso" ?
-                <Invoice id={`INC-${index}`} ammount={item.amount} date={item.date}
-                  description={item.description}
-                  type={item.type}
-                /> :
-                <Invoice id={`EXP-${index}`} ammount={item.amount} date={item.date}
-                  description={item.description}
-                  type={item.type}
-                /> : null
+          {data.slice(-3).map((item, index) => (
+            item.type === "Ingreso" ?
+              <Invoice id={item.id} ammount={item.amount} date={item.date}
+                description={item.description}
+                type={item.type}
+              /> :
+              <Invoice id={item.id} ammount={item.amount} date={item.date}
+                description={item.description}
+                type={item.type}
+              />
           ))}
         </VuiBox>
       </VuiBox>
