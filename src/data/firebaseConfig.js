c0b9f3e010211861ad4402752a0a848c5b25853c
Jorge Carranza <73572833+JorgeCQ07@users.noreply.github.com>
2023-10-16 19:48:44 -0600

    agrega auth

--- src/data/firebaseConfig.js
+++ src/data/firebaseConfig.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,4 +24,8 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getDatabase(app);
 
+// Initialize Auth
+const auth = getAuth();
+
 export default db;
+export { auth };
